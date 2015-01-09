620a7917001d1db6a9e96a6d445751c0ae3e915d
ccabanero <clintcabanero@gmail.com>
2015-01-08 16:05:34 -0800

    adding mapbox android sdk; initializing map with OSM tile layer

--- OpenMapkit/app/src/main/java/americanredcross/org/openmapkit/MapActivity.java
+++ OpenMapkit/app/src/main/java/americanredcross/org/openmapkit/MapActivity.java
@@ -1,19 +1,54 @@
 package americanredcross.org.openmapkit;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.tileprovider.tilesource.WebSourceTileLayer;
+import com.mapbox.mapboxsdk.views.MapView;
+
 
 public class MapActivity extends ActionBarActivity {
+
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        initializeMap();
     }
 
+    /**
+     * For initializing the map with default tile layer, location, extent, and zoom level
+     */
+    private void initializeMap() {
+
+        // instantiate map
+        this.mapView = (MapView)findViewById(R.id.mapView);
+
+        // set the  default map tile layer (OSM)
+        String defaultTilePID = getString(R.string.defaultTileLayerPID);
+        String defaultTileURL = getString(R.string.defaultTileLayerURL);
+        String defaultTileName = getString(R.string.defaultTileLayerName);
+        String defaultTileAttribution = getString(R.string.defaultTileLayerAttribution);
+
+        WebSourceTileLayer ws = new WebSourceTileLayer(defaultTilePID, defaultTileURL);
+        ws.setName(defaultTileName)
+                .setAttribution(defaultTileAttribution)
+                .setMinimumZoomLevel(1)
+                .setMaximumZoomLevel(18);
+
+        mapView.setTileSource(ws);
+
+        // set default map extent and zoom
+        LatLng initialCoordinate = new LatLng(23.728791, 90.409412);
+        mapView.setCenter(initialCoordinate);
+        mapView.setZoom(12);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
