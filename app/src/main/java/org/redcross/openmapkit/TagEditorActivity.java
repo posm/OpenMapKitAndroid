301a07542e72a20afe3e87d04a5b373d7d7472df
ccabanero <clintcabanero@gmail.com>
2015-01-13 16:20:04 -0800

    user interface for OSM Tag Editor activity

--- app/src/main/java/org/redcross/openmapkit/TagEditorActivity.java
+++ app/src/main/java/org/redcross/openmapkit/TagEditorActivity.java
@@ -2,13 +2,28 @@ package org.redcross.openmapkit;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 public class TagEditorActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_tag_editor);
+
+        Spinner spinner = (Spinner) findViewById(R.id.existingTagsSpinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.testTags, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
