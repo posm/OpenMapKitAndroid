e4fa00336760635a77c0b30a2af2586de5e60e00
Nicholas Hallahan <nhallahan@spatialdev.com>
2015-03-03 18:20:03 -0800

    working on SelectOneTagValueFragment

--- app/src/main/java/org/redcross/openmapkit/tagswipe/SelectOneTagValueFragment.java
+++ app/src/main/java/org/redcross/openmapkit/tagswipe/SelectOneTagValueFragment.java
@@ -1,9 +1,8 @@
 package org.redcross.openmapkit.tagswipe;
 
-import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -72,17 +71,6 @@ public class SelectOneTagValueFragment extends Fragment {
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
         }
     }
 
--- app/src/main/java/org/redcross/openmapkit/tagswipe/TagSwipeActivity.java
+++ app/src/main/java/org/redcross/openmapkit/tagswipe/TagSwipeActivity.java
@@ -90,9 +90,11 @@ public class TagSwipeActivity extends ActionBarActivity {
         @Override
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-//            return PlaceholderFragment.newInstance(position + 1);
-            return StringTagValueFragment.newInstance("string won", "string too");
+            if (position == 0) {
+                return StringTagValueFragment.newInstance("string won", "string too");
+            } else {
+                return SelectOneTagValueFragment.newInstance("string won", "string too");
+            }
         }
 
         @Override
@@ -113,42 +115,6 @@ public class TagSwipeActivity extends ActionBarActivity {
                     return getString(R.string.title_section3).toUpperCase(l);
             }
             return null;
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_tag_swipe, container, false);
-
-            ((TextView) rootView.findViewById(R.id.section_label)).setText("test value");
-            
-            return rootView;
         }
     }
 
