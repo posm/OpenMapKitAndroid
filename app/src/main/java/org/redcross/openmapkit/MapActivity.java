b3caffe42121ad072c2d64a8e077e0883233eb4a
clintcabanero <clintcabanero@gmail.com>
2015-03-05 06:52:49 -0800

    adding changes from remote

--- app/src/main/java/org/redcross/openmapkit/MapActivity.java
+++ app/src/main/java/org/redcross/openmapkit/MapActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
