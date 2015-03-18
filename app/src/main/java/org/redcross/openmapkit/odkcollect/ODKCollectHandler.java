4a366a1f387947d2dcd0acd825587db182c155fd
Nicholas Hallahan <nhallahan@spatialdev.com>
2015-03-18 13:51:38 -0700

    Fixes #29

--- app/src/main/java/org/redcross/openmapkit/odkcollect/ODKCollectHandler.java
+++ app/src/main/java/org/redcross/openmapkit/odkcollect/ODKCollectHandler.java
@@ -35,11 +35,9 @@ public class ODKCollectHandler {
                     String instanceDir = extras.getString("INSTANCE_DIR");
                     LinkedHashMap<String, ODKTag> requiredTags = generateRequiredOSMTagsFromBundle(extras);
                     odkCollectData = new ODKCollectData(formId, formFileName, instanceId, instanceDir, requiredTags);
-                    return;
-                } 
+                }
             }
         }
-        odkCollectData = null;
     }
     
     public static boolean isODKCollectMode() {
