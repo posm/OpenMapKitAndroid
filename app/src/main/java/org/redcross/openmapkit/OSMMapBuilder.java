b26cbdddcab9bf82e2ae3f02e1d3f12cf7cb8fa7
Nicholas Hallahan <nhallahan@spatialdev.com>
2015-04-01 16:26:42 -0700

    Closes #54

--- app/src/main/java/org/redcross/openmapkit/OSMMapBuilder.java
+++ app/src/main/java/org/redcross/openmapkit/OSMMapBuilder.java
@@ -180,7 +180,7 @@ public class OSMMapBuilder extends AsyncTask<File, Long, JTSModel> {
         progressDialog.setTitle("Loading OSM Data");
         progressDialog.setMessage("");
         progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-        progressDialog.setCancelable(false);
+//        progressDialog.setCancelable(false);
         progressDialog.setProgress(0);
         progressDialog.setMax(100);
         progressDialog.show();
