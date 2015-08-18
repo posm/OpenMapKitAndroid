7314557e25dc2c8a9e6b91690a483b48da921ab8
imwongela <imwongela@ona.io>
2015-08-18 12:04:02 +0300

    Add new source files to the repo

---
+++ app/src/main/java/org/redcross/openmapkit/ProximityMapView.java
@@ -0,0 +1,130 @@
+package org.redcross.openmapkit;
+
+import android.content.Context;
+import android.graphics.PointF;
+import android.location.Location;
+import android.util.AttributeSet;
+
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.overlay.GpsLocationProvider;
+import com.mapbox.mapboxsdk.overlay.UserLocationOverlay;
+import com.mapbox.mapboxsdk.tileprovider.MapTileLayerBase;
+import com.mapbox.mapboxsdk.views.MapView;
+import com.mapbox.mapboxsdk.views.util.Projection;
+
+import org.redcross.openmapkit.proximity.GpsProximityProvider;
+import org.redcross.openmapkit.proximity.UserProximityOverlay;
+
+/**
+ * Created by coder on 8/12/15.
+ */
+public class ProximityMapView extends MapView {
+    private UserProximityOverlay mLocationOverlay;
+
+    public ProximityMapView(final Context aContext) {
+        super(aContext, 256, null, null, null);
+    }
+
+    public ProximityMapView(final Context aContext, AttributeSet attrs) {
+        super(aContext, 256, null, null, attrs);
+    }
+
+    protected ProximityMapView(Context aContext, int tileSizePixels, MapTileLayerBase aTileProvider) {
+        super(aContext, tileSizePixels, aTileProvider, null, null);
+    }
+
+    private UserProximityOverlay getOrCreateLocationOverlay() {
+        if (mLocationOverlay == null) {
+            mLocationOverlay = new UserProximityOverlay(new GpsProximityProvider(getContext()), this);
+            addOverlay(mLocationOverlay);
+        }
+        return mLocationOverlay;
+    }
+
+    /**
+     * Show the user location overlay
+     */
+    public MapView setUserLocationEnabled(final boolean value) {
+        getOrCreateLocationOverlay().enableMyLocation();
+        return this;
+    }
+
+    /**
+     * Show or hide the user location overlay
+     */
+    public final boolean getUserProximityEnabled() {
+        if (mLocationOverlay != null) {
+            return mLocationOverlay.isMyLocationEnabled();
+        }
+        return false;
+    }
+
+    /**
+     * Set the user location tracking mode
+     */
+    public MapView setUserLocationTrackingMode(final UserProximityOverlay.TrackingMode mode) {
+        getOrCreateLocationOverlay().setTrackingMode(mode);
+        return this;
+    }
+
+    /**
+     * Set the user location tracking zoom level
+     */
+    public MapView setUserLocationRequiredZoom(final float zoomLevel) {
+        getOrCreateLocationOverlay().setRequiredZoom(zoomLevel);
+        return this;
+    }
+
+    /**
+     * get the user location tracking mode
+     */
+    public UserProximityOverlay.TrackingMode getUserProximityTrackingMode() {
+        if (mLocationOverlay != null) {
+            return mLocationOverlay.getTrackingMode();
+        }
+        return UserProximityOverlay.TrackingMode.NONE;
+    }
+
+    /**
+     * Go to user location
+     */
+    public void goToUserLocation(final boolean animated) {
+        if (mLocationOverlay != null) {
+            mLocationOverlay.goToMyPosition(animated);
+        }
+    }
+
+    /**
+     * Get the user location overlay if created
+     */
+    public UserProximityOverlay getUserProximityOverlay() {
+        return mLocationOverlay;
+    }
+
+    /**
+     * Get the user location overlay if created
+     */
+    public LatLng getUserLocation() {
+        if (mLocationOverlay != null) {
+            return mLocationOverlay.getMyLocation();
+        }
+        return null;
+    }
+
+    public boolean isUserLocationVisible() {
+        if (mLocationOverlay != null) {
+            final Location pos = mLocationOverlay.getLastFix();
+            if (pos != null && isLayedOut()) {
+                final Projection projection = getProjection();
+                final float accuracyInPixels = pos.getAccuracy() / (float) projection.groundResolution(
+                        pos.getLatitude());
+                final PointF point = projection.toMapPixels(pos.getLatitude(), pos.getLongitude(), null);
+                return projection.getScreenRect().intersects((int) (point.x - accuracyInPixels),
+                        (int) (point.y - accuracyInPixels),
+                        (int) (point.x + accuracyInPixels),
+                        (int) (point.y + accuracyInPixels));
+            }
+        }
+        return false;
+    }
+}
