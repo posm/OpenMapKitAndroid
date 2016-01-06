397215f7ea34e4f1e67a1bac403891d23885688c
Nicholas Hallahan <nick@theoutpost.io>
2016-01-06 06:30:35 -0800

    tag key and tag label text views are populated #103

--- app/src/main/java/org/redcross/openmapkit/tagswipe/SelectMultipleTagValueFragment.java
+++ app/src/main/java/org/redcross/openmapkit/tagswipe/SelectMultipleTagValueFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import org.redcross.openmapkit.R;
 
@@ -41,6 +42,24 @@ public class SelectMultipleTagValueFragment extends Fragment {
     }
 
     private void setupWidgets() {
+        TextView tagKeyLabelTextView = (TextView) rootView.findViewById(R.id.tagKeyLabelTextView);
+        TextView tagKeyTextView = (TextView) rootView.findViewById(R.id.tagKeyTextView);
+
+        String keyLabel = tagEdit.getTagKeyLabel();
+        String key = tagEdit.getTagKey();
+
+        if (keyLabel != null) {
+            tagKeyLabelTextView.setText(keyLabel);
+            tagKeyTextView.setText(key);
+        } else {
+            tagKeyLabelTextView.setText(key);
+            tagKeyTextView.setText("");
+        }
+
+        setupCheckBoxes();
+    }
+
+    private void setupCheckBoxes() {
 
     }
 
