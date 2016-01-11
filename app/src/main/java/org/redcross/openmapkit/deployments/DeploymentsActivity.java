03e6cdff7ee6a1afa148a6c083ab6a27681cfcfb
Nicholas Hallahan <nick@theoutpost.io>
2016-01-11 11:29:01 -0800

    download fab in deployment details #122

--- app/src/main/java/org/redcross/openmapkit/deployments/DeploymentsActivity.java
+++ app/src/main/java/org/redcross/openmapkit/deployments/DeploymentsActivity.java
@@ -1,30 +1,17 @@
 package org.redcross.openmapkit.deployments;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.redcross.openmapkit.R;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 
 public class DeploymentsActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
