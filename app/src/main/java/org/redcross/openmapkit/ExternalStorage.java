b4e67eaff58654f23b7559246e0ed98da30a84f7
Nicholas Hallahan <nick@theoutpost.io>
2016-04-09 10:24:44 -0700

    #19  #135 get storage directories, including SD card.

--- app/src/main/java/org/redcross/openmapkit/ExternalStorage.java
+++ app/src/main/java/org/redcross/openmapkit/ExternalStorage.java
@@ -2,7 +2,15 @@ package org.redcross.openmapkit;
 
 import android.os.Environment;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 /**
  * For encapsulating tasks such as checking if external storage is available for read, for write, and fetching files from storage
@@ -114,6 +122,71 @@ public class ExternalStorage {
         }
         return targetFile;
     }
-    
+
+    /**
+     * From
+     * https://github.com/mstorsjo/vlc-android/blob/master/vlc-android/src/org/videolan/vlc/util/AndroidDevices.java
+     *
+     * @return storage directories, including external SD card
+     */
+    public static String[] getStorageDirectories() {
+        String[] dirs = null;
+        BufferedReader bufReader = null;
+        ArrayList<String> list = new ArrayList<String>();
+        list.add(Environment.getExternalStorageDirectory().getPath());
+
+        List<String> typeWL = Arrays.asList("vfat", "exfat", "sdcardfs", "fuse");
+        List<String> typeBL = Arrays.asList("tmpfs");
+        String[] mountWL = { "/mnt", "/Removable" };
+        String[] mountBL = {
+                "/mnt/secure",
+                "/mnt/shell",
+                "/mnt/asec",
+                "/mnt/obb",
+                "/mnt/media_rw/extSdCard",
+                "/mnt/media_rw/sdcard",
+                "/storage/emulated" };
+        String[] deviceWL = {
+                "/dev/block/vold",
+                "/dev/fuse",
+                "/mnt/media_rw/extSdCard" };
+
+        try {
+            bufReader = new BufferedReader(new FileReader("/proc/mounts"));
+            String line;
+            while((line = bufReader.readLine()) != null) {
+
+                StringTokenizer tokens = new StringTokenizer(line, " ");
+                String device = tokens.nextToken();
+                String mountpoint = tokens.nextToken();
+                String type = tokens.nextToken();
+
+                // skip if already in list or if type/mountpoint is blacklisted
+                if (list.contains(mountpoint) || typeBL.contains(type) || Strings.StartsWith(mountBL, mountpoint))
+                    continue;
+
+                // check that device is in whitelist, and either type or mountpoint is in a whitelist
+                if (Strings.StartsWith(deviceWL, device) && (typeWL.contains(type) || Strings.StartsWith(mountWL, mountpoint)))
+                    list.add(mountpoint);
+            }
+
+            dirs = new String[list.size()];
+            for (int i = 0; i < list.size(); i++) {
+                dirs[i] = list.get(i);
+            }
+        }
+        catch (FileNotFoundException e) {}
+        catch (IOException e) {}
+        finally {
+            if (bufReader != null) {
+                try {
+                    bufReader.close();
+                }
+                catch (IOException e) {}
+            }
+        }
+        return dirs;
+    }
+
 }
 
